e9677389602425e8fac1db66d517aef16aa29aa7
Rui Carvalho <rhwy@users.noreply.github.com>
2017-09-23 23:44:55 +0200

    Update SurveyCalculations.cs

--- bot/telly/SurveyCalculations.cs
+++ bot/telly/SurveyCalculations.cs
@@ -3,7 +3,7 @@ using System;
 public class SurveyCalculations
 {
     private static bool weAreInDebugMode = true;
-    public Tuple<bool,bool> ShowGenderCounterResult(double totalVotes, int globalPercent, int yourChoicePercent )
+    public Tuple<bool,bool> ShowGenderCounterResult(double totalVotes, int globalPercent, int yourChoicePercent, int alternateChoicePercent )
     {
         var currentShowStatus = false;
         currentShowStatus = 
@@ -11,9 +11,9 @@ public class SurveyCalculations
             (totalVotes >= 20) 
             //AND if (
             //  ABS(yourChoice percent - otherchoice percent) >= 8%
-            && (Math.Abs(50-yourChoicePercent) >= 4 
-            //  OR yourChoicePercent >= 25% of global percent 
-                || ((Math.Abs(globalPercent-yourChoicePercent)/globalPercent*100)>25));
+            && (Math.Abs(alternateChoicePercent-yourChoicePercent) >= 8 
+            //  OR yourChoicePercent >= 25% of the alternate choice percent 
+                || ((Math.Abs(yourChoicePercent-alternateChoicePercent)/yourChoicePercent*100)>25));
             // then show the result
 
         return Tuple.Create(currentShowStatus,weAreInDebugMode);
