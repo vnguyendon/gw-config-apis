73912292331176359b2d3c4cf147e881f52ea913
Rui Carvalho <rhwy@users.noreply.github.com>
2017-09-23 19:41:41 +0200

    Update SurveyCalculations.cs

--- bot/telly/SurveyCalculations.cs
+++ bot/telly/SurveyCalculations.cs
@@ -1,4 +1,21 @@
-public class Test
+using System;
+
+public class SurveyCalculations
 {
-  public ResultType Say() => new ResultType{ Value="Hello World From github"};
+    private static bool weAreInDebugMode = true;
+    public Tuple<bool,bool> ShowGenderCounterResult(double totalVotes, int globalPercent, int yourChoicePercent )
+    {
+        var currentShowStatus = false;
+        currentShowStatus = 
+            //if there is more than 20 votes in total 
+            (totalVotes >= 20) 
+            //AND if (
+            //  ABS(yourChoice percent - otherchoice percent) >= 8%
+            && (Math.Abs(50-yourChoicePercent) >= 4 
+            //  OR yourChoicePercent >= 25% of global percent 
+                || ((Math.Abs(globalPercent-yourChoicePercent)/globalPercent*100)>25));
+            // then show the result
+
+        return Tuple.Create(currentShowStatus,weAreInDebugMode);
+    } 
 }
