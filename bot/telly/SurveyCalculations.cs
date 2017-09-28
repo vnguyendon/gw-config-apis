e8120634bbb612d850d819fb074bd92d5c2845fe
Rui Carvalho <rhwy@users.noreply.github.com>
2017-09-28 09:08:01 +0200

    Update SurveyCalculations.cs

--- bot/telly/SurveyCalculations.cs
+++ bot/telly/SurveyCalculations.cs
@@ -1,21 +1,34 @@
-using System;
-
-public class SurveyCalculations
+public class SurveyShowConditions : ISurveyShowConditions
 {
-    private static bool weAreInDebugMode = true;
-    public Tuple<bool,bool> ShowGenderCounterResult(double totalVotes, double globalPercent, double yourChoicePercent, double alternateChoicePercent )
-    {
-        var currentShowStatus = false;
-        currentShowStatus = 
-            //if there is more than 20 votes in total 
-            (totalVotes >= 20) 
-            //AND if (
-            //  ABS(yourChoice percent - otherchoice percent) >= 8%
-            && (Math.Abs(alternateChoicePercent-yourChoicePercent) >= 8 
-            //  OR yourChoicePercent >= 25% of the alternate choice percent 
-                || ((Math.Abs(yourChoicePercent-alternateChoicePercent)/yourChoicePercent*100)>25));
-            // then show the result
+    private int deltaPercent = 8;
+    private int minVotes = 20;
 
-        return Tuple.Create(currentShowStatus,weAreInDebugMode);
-    } 
+    public bool IsDebugModeActivated { get; } = true;
+    public bool ShowPercentAgeRange(CounterValues ageRangeCounter, CounterValues globalCounter)
+    {
+        return (Math.Abs(globalCounter.YourPercentage - ageRangeCounter.YourPercentage) >= deltaPercent)
+                && (ageRangeCounter.Total >= minVotes);
+    }
+
+    public bool ShowPercentCountry(CounterValues countryCounter, CounterValues globalCounter)
+    {
+        return (Math.Abs(globalCounter.YourPercentage - countryCounter.YourPercentage) >= deltaPercent)
+                && (countryCounter.Total >= minVotes);
+    }
+
+    public bool ShowPercentGender(CounterValues genderCounter, CounterValues globalCounter)
+    {
+        return (Math.Abs(globalCounter.YourPercentage - genderCounter.YourPercentage) >= deltaPercent)
+                && (genderCounter.Total >= minVotes);
+    }
+
+    public bool ShowPercentVoters(CounterValues globalCounter)
+    {
+        return true;
+    }
+
+    public bool ShowTotalVoters(CounterValues globalCounter)
+    {
+        return globalCounter.Total >= minVotes;
+    }
 }
